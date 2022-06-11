ea5464160b7cd44d1a1ecefcf78f41bd53fea03d
Goutham P Hegde <59412269+gouthamphegde@users.noreply.github.com>
2022-06-11 19:14:02 +0530

    Time: 228 ms (72.62%), Space: 98.6 MB (71.00%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,6 +2,14 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         
+        
+        //Intution
+        //Now if you are thinking since I have to either pop left or right we can do recursion
+        //which gives O(2^n) complexity and with dp we can finally come down to O(n*n) maybe
+        // now instead of that think of it as a sliding window problem
+        // where sum - target = z where z is sum of middle subarray which dosen't have to be 
+        // removed. We have to find such largest subarray
+        
         int sum = 0;
         int c = 0 , l = 0 , j = 0 , i = 0;
         int s = 0;
