a3f0bcb55b0cd3e7a8ab969c6c714083dab857f7
Goutham P Hegde <59412269+gouthamphegde@users.noreply.github.com>
2022-07-06 10:42:48 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.05%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0){
+            return 0;
+        }
+        
+        int a = 0;
+        int b = 1;
+        int ans = 0;
+        
+        for(int i = 2 ; i <= n ;i++){
+            ans = a + b;
+            a = b;
+            b = ans;
+        }
+        
+        
+        return b;
+        
+    }
+};
