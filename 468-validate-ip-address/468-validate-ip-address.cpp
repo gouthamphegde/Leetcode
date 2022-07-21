b8ab6a72e4a41a80c26a7452aa257b8c7ca2a83b
Goutham P Hegde <59412269+gouthamphegde@users.noreply.github.com>
2022-07-22 00:11:20 +0530

    Time: 6 ms (22.12%), Space: 6.4 MB (31.28%) - LeetHub

--- 468-validate-ip-address/468-validate-ip-address.cpp
+++ 468-validate-ip-address/468-validate-ip-address.cpp
@@ -1,13 +1,63 @@
 class Solution {
 public:
     string validIPAddress(string queryIP) {
-        regex ipv4("(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])"), ipv6("((([0-9a-fA-F]){1,4})\\:){7}([0-9a-fA-F]){1,4}");   // create regex object for regulare expression
-        if(regex_match(queryIP, ipv4)) // match regex expression with given IP string for IPv4
-            return "IPv4";
-        else if(regex_match(queryIP, ipv6)) // match regex expression with given IP string for IPv6
-            return "IPv6";
-        return "Neither"; // Otherwise return "Neither"
-
-        
+        return validIPv4(queryIP) ? "IPv4" : (validIPv6(queryIP) ? "IPv6" : "Neither");
     }
+private:
+    bool validIPv4(string IP) {
+        if (count(IP.begin(), IP.end(), '.') != 3) {
+            return false;
+        }
+        vector<string> parts = split(IP, '.');
+        if (parts.size() != 4) {
+            return false;
+        }
+        for (string part : parts) {
+            if (part.empty() || part.size() > 3 || part.size() > 1 && part[0] == '0') {
+                return false;
+            }
+            for (char c : part) {
+                if (!isdigit(c)) {
+                    return false;
+                }
+            }
+            if (stoi(part) > 255) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    bool validIPv6(string IP) {
+        if (count(IP.begin(), IP.end(), ':') != 7) {
+            return false;
+        }
+        vector<string> parts = split(IP, ':');
+        if (parts.size() != 8) {
+            return false;
+        }
+        for (string part : parts) {
+            if (part.empty() || part.size() > 4) {
+                return false;
+            }
+            for (char c : part) {
+                if (!isdigit(c) && (!isalpha(c) || toupper(c) > 'F')) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    vector<string> split(string s, char c) {
+        vector<string> parts;
+        string part;
+        istringstream in(s);
+        while (getline(in, part, c)) {
+            parts.push_back(part);
+        }
+        return parts;
+    }
+
+
 };
