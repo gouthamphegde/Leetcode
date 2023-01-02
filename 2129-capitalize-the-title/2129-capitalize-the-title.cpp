c7022a4a161193b089dd99c7451fb886a0989ce3
Goutham P Hegde <59412269+gouthamphegde@users.noreply.github.com>
2023-01-02 09:18:32 +0530

    Time: 2 ms (76.17%), Space: 6.5 MB (22.77%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.cpp
+++ 2129-capitalize-the-title/2129-capitalize-the-title.cpp
@@ -1,76 +1,30 @@
 class Solution {
 public:
     string capitalizeTitle(string title) {
-        vector<string>words;
-        string ans = "";
+        istringstream iss(title);   //or use stringstream
+        string word;
+        string str="" ;
+        
 
-        int i;
-        string word="";
-        for(i=0;i<title.length();i++){
-            if(title[i]==' '){
-                if(word.length() == 1 && word[0] <= 'Z'){
-                word[0] = tolower(word[0]);
-            }
-            if(word.length() == 2){
-                word[0] = tolower(word[0]);
-                word[1] = tolower(word[1]);
- 
+        while(iss>>word){
+            //  cout<<word<<" ";
+            if(word.length()<=2){
+                transform(word.begin(), word.end(), word.begin(), ::tolower);
+                str+=word+" ";
             }
             else{
-                transform(word.begin(), word.end(), word.begin(), ::tolower);
                 
-                if(word.length() == 1){
-                    
-                }
-                else{
-                                    word[0] = word[0] - 32;
-
-                }
+                word[0]=toupper(word[0]);
+                transform(word.begin()+1, word.end(), word.begin()+1, ::tolower);
                 
 
-            }
-            ans = ans + word;
-            
-            ans = ans+' ';
+                str+= word+" ";
                 
-            word="";
-            }else{
-                word+=title[i];
             }
         }
-        
-        if(word.length() == 1 || word.length() == 2){
-        transform(word.begin(), word.end(), word.begin(), ::tolower);
+        // cout<<str;
+        //   rtrim(str);
 
- 
-            }
-            else{
-                transform(word.begin(), word.end(), word.begin(), ::tolower);
-                 word[0] = word[0] - 32;
-
-            }
-        ans+=word;
-
-        
-        /*for(auto word : words){
-            if(word.length() == 1){
-                word[0] = word[0] - 32;
-            }
-            if(word.length() == 2){
-                word[0] = word[0] - 32;
-                word[1] = word[1] - 32;
- 
-            }
-            else{
-                transform(word.begin(), word.end(), word.begin(), ::tolower);
-                 word[0] = word[0] - 32;
-
-            }
-            ans = ans + word;
-            
-            ans = ans+' ';
-        }*/
-        
-        return ans;
+        return str.substr(0,str.length()-1);
     }
 };
